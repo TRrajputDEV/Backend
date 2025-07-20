cd457020548162fc1366dcb6fd1b39911481184e
TRrajputDev <tushartanwar183@gmail.com>
2025-07-20 14:09:10 +0530

    Phase-2 DB connection successfull

--- phase2/src/index.js
+++ phase2/src/index.js
@@ -1,11 +1,17 @@
 import dotenv from 'dotenv';
 import { app } from './app.js';
+import connect_DB from './db/index.js'
 
 dotenv.config({
-        path: './env'
-    })
+    path: './env'
+})
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running at port ${process.env.PORT}`)
+connect_DB()
+.then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running at port ${process.env.PORT}`)
+        })
+})
+.catch((err)=>{
+    console.log("connection with MongoDB failed !!", err);
 })
